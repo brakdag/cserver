5c1f3f7e56566fc596d1bd4ec4ec06d2198ec07a
brakdag <brakdag@gmail.com>
2020-04-28 22:33:30 -0300

    a

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const convert = require('../lib/convert')
+
 router.get('/getlist', function (req, res) {
     convert.getlist(x=>res.send(x))
 });
