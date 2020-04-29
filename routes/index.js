9b1c6a1b2ddf490546afea2977c2c98fcc2295d1
brakdag <brakdag@gmail.com>
2020-04-28 23:39:33 -0300

    cambios

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,16 @@ router.get('/getlist', function (req, res) {
     convert.getlist(function(x){res.send(x)})
 })
 
+router.get('/convert', function (req, res) {
+    convert.convert(req.query.filename,function(x){res.send(x)})
+})
+
+router.get('/delfile', function (req, res) {
+    convert.delfile(req.query.filename,function(x){res.send(x)})
+})
+
+
+
 router.get('/', (req, res) => { res.render('index.ejs') })
 
 module.exports = router
